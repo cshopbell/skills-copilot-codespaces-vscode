5caa91fcfbe07bb206162704601ed99134f1b301
cshopbell <61161208+cshopbell@users.noreply.github.com>
2024-07-15 23:10:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,82 +1,16 @@
-//create web server
-//create 2 routes
-// 1. /comments
-// 2. /comments/new
-// 3. /comments/delete
-// 4. /comments/edit
-// 5. /comments/show
-
-//1. /comments
-//  - show all comments
-//  - create a form to add a new comment
-//2. /comments/new
-//  - add a new comment
-//  - redirect to /comments
-//3. /comments/delete
-//  - delete a comment
-//  - redirect to /comments
-//4. /comments/edit
-//  - edit a comment
-//  - redirect to /comments
-//5. /comments/show
-//  - show a single comment
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const methodOverride = require('method-override');
-const {v4:uuid} = require('uuid');
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(methodOverride('_method'));
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-
-let comments = [
-    {
-        id:uuid(),
-        username:'Todd',
-        comment:'lol that is so funny'
-    },
-    {
-        id:uuid(),
-        username:'Skyler',
-        comment:'I like to go birdwatching with my dog'
-    },
-    {
-        id:uuid(),
-        username:'Sk8erBoi',
-        comment:'Plz delete your account Todd'
-    },
-    {
-        id:uuid(),
-        username:'onlysayswoof',
-        comment:'woof woof woof'
-    }
-]
-
-app.get('/comments',(req,res)=>{
-    res.render('comments/index',{comments});
-})
-
-app.get('/comments/new',(req,res)=>{
-    res.render('comments/new');
-})
-
-app.post('/comments',(req,res)=>{
-    const {username,comment} = req.body;
-    comments.push({username,comment,id:uuid()});
-    res.redirect('/comments');
-})
-
-app.get('/comments/:id',(req,res)=>{
-    const {id} = req.params;
-    const comment = comments.find(c => c.id === id);
-    res.render('comments/show',{comment});
-})
-
-app.get('/comments/:id/edit',(req,res)=>{
-    const {id} = req.params;
-    const comment = comments.find(c => c.id === id);
-    res.render('comments/edit',{
+// create web server
+// run: node comments.js
+// open browser and type: http://localhost:3000/
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var comments = [];
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+    res.json(comments);
+});
+app.post('/', function(req, res){
+    comments.push(req.body);
+    res.json(comments);
+});
+app.listen(3000);
